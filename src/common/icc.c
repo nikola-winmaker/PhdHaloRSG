793256fef9f125cf21cb96b2e16d943d9ca85b63
Milos Kovac <cikakova997@gmail.com>
2026-05-10 18:09:35 +0200

    Icc intro

---
+++ src/common/icc.c
@@ -0,0 +1,47 @@
+#include "icc.h"
+#include "string.h"
+
+t_memoryMap memoryMap[5] =
+{
+    {(uint64_t*)SENSOR_FRAME_BASE,    SENSOR_FRAME_SIZE}, //SensorFrameIf
+    {(uint64_t*)CHARGE_COMMAND_BASE,  CHARGE_COMMAND_SIZE},
+    {(uint64_t*)CHARGE_STATUS_BASE,   CHARGE_STATUS_SIZE},
+    {(uint64_t*)SAFETY_STATE_BASE,    SAFETY_STATE_SIZE},
+    {(uint64_t*)OPERATOR_COMMAND_BASE,OPERATOR_COMMAND_SIZE}
+};
+
+int writeData(t_Channel channel,void* payload,uint32_t size)
+{
+    int ret = 0;
+    t_iccInstance* iccInstance = (t_iccInstance*)&memoryMap[channel].p_dataStart[0];
+    if (iccInstance->readCnt == iccInstance->writeCnt)
+    {
+        //first uint64 reserved for pointers
+        uint64_t* dest = &memoryMap[channel].p_dataStart[1];
+        //only in this case we can prepare palyoad
+        memcpy(dest,payload,size);
+
+        iccInstance->writeCnt++;
+
+        ret = 1;
+    }
+
+    return ret;
+}
+
+int readData(t_Channel channel,void* payload,uint32_t size)
+{
+    int ret = 0;
+    t_iccInstance* iccInstance = (t_iccInstance*)&memoryMap[channel].p_dataStart[0];
+    //is there something to read
+    if (iccInstance->readCnt != iccInstance->writeCnt)
+    {
+        uint64_t* src = &memoryMap[channel].p_dataStart[1];
+        //new data 
+        memcpy(payload,src,size);
+
+        ret = 1;
+    }
+
+    return ret; 
+}
---
+++ src/common/icc.h
@@ -0,0 +1,67 @@
+#ifndef ICC_H
+#define ICC_H 
+
+#include "memory_layout.h"
+#include "stdint.h"
+
+typedef enum
+{
+    channel_sensorFrame = 0,
+    channel_chargeCommand,
+    channel_ChargeStatus,
+    channel_SafetyState,
+    channel_OperatorCommand
+}t_Channel;
+
+typedef struct
+{
+    uint64_t* p_dataStart;
+    uint32_t dataSize; 
+}t_memoryMap;
+
+typedef struct
+{
+    unsigned int writeCnt;
+    unsigned int readCnt;
+}t_iccInstance;
+
+//Specific data 
+typedef struct SensorFrameData {
+    uint32_t battery_voltage_mv;
+    int32_t charge_current_ma;
+    float battery_temp_c;
+    uint8_t breaker_closed;
+    uint32_t fault_flags;
+} SensorFrameData;
+
+typedef struct OperatorCommandData {
+    uint32_t command_id;
+    uint32_t command_param;
+} OperatorCommandData;
+
+typedef struct SafetyStateData {
+    uint8_t safe_mode;
+    uint8_t breaker_open;
+    uint8_t charging_allowed;
+    uint32_t heartbeat_counter;
+} SafetyStateData;
+
+typedef struct ChargeStatusData {
+    char charger_state[5];
+    uint32_t requested_current_ma;
+    uint32_t requested_voltage_mv;
+    uint32_t fault_state;
+} ChargeStatusData;
+
+typedef struct ChargeCommandData {
+    uint8_t enable_charging;
+    uint32_t current_limit_ma;
+    uint32_t voltage_limit_mv;
+    char charging_mode[7];
+} ChargeCommandData;
+
+int writeData(t_Channel channel,void* payload,uint32_t size);
+
+int readData(t_Channel channel,void* payload,uint32_t size);
+
+#endif
