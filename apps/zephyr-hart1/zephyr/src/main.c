7ff9cbf8cdb1a6d716326337f9efa94fafd99f40
Bosko Kragulj <bosko.kragulj@comdata.rs>
2026-05-10 15:03:57 +0000

    Add changes for SensorFrame read on Zephyr

--- apps/zephyr-hart1/zephyr/src/main.c
+++ apps/zephyr-hart1/zephyr/src/main.c
@@ -11,6 +11,9 @@
 
 /************************* INCLUDE SECTION *************************/
 #include <zephyr/kernel.h>
+#include <stdatomic.h> //added
+#include <string.h> //added
+#include "memory_layout.h" //added
 #include "workshop_protocol.h"
 #include "console_lock.h"
 #include "bms.h"
@@ -20,12 +23,108 @@
     #include "halo_api.h"
 #endif
 
+typedef struct
+{
+    volatile uint32_t status;
+
+} MailboxHeader;
+
+#define SENSOR_STATUS \
+    (*(volatile uint32_t*)SENSOR_FRAME_BASE)
+
+#define SENSOR_DATA \
+    ((volatile SensorFrame*)(SENSOR_FRAME_BASE + sizeof(uint32_t)))
+
 /************************* GLOBAL SECTION *************************/
 
-//TODO Classical: Define SensorFrame structure based on the workshop specification only for the classical implementation!. 
+//TODO Classical: 0. Define SensorFrame structure based on the workshop specification only for the classical implementation!. 
+typedef struct {
+    uint32_t battery_voltage_mv;
+    int32_t charge_current_ma;
+    float battery_temp_c;
+    uint8_t breaker_closed;
+    uint32_t fault_flags;
+} SensorFrame;
 
+/*typedef struct
+{
+    atomic_uint status;
+    SensorFrame sensor_frame;
+} SharedSensorRegion;
+
+// Shared memory mapped structure
+volatile SharedSensorRegion *shared_region =
+    (volatile SharedSensorRegion *)SENSOR_FRAME_BASE;*/
 
 /************************* FUNCTION SECTION *************************/
+typedef enum
+{
+    IPC_IDLE = 0,
+    IPC_WRITING = 1,
+    IPC_READING = 2
+
+} IpcStatus;
+
+static bool ipc_write(volatile uint32_t* status,
+                      volatile void* destination,
+                      const void* source,
+                      uint32_t size)
+{
+    if (*status != IPC_IDLE)
+    {
+        return false;
+    }
+
+    *status = IPC_WRITING;
+
+    memcpy((void*)destination,
+           source,
+           size);
+
+    *status = IPC_IDLE;
+
+    return true;
+}
+
+static bool ipc_read(volatile uint32_t* status,
+                     const volatile void* source,
+                     void* destination,
+                     uint32_t size)
+{
+    if (*status != IPC_IDLE)
+    {
+        return false;
+    }
+
+    *status = IPC_READING;
+
+    memcpy(destination,
+           (const void*)source,
+           size);
+
+    *status = IPC_IDLE;
+
+    return true;
+}
+
+bool write_sensor_frame(const SensorFrame* data)
+{
+    return ipc_write(
+        &SENSOR_STATUS,
+        SENSOR_DATA,
+        data,
+        sizeof(SensorFrame));
+}
+
+bool read_sensor_frame(SensorFrame* data)
+{
+    return ipc_read(
+        &SENSOR_STATUS,
+        SENSOR_DATA,
+        data,
+        sizeof(SensorFrame));
+}
+
 int main( void )
 {
 
@@ -93,6 +192,7 @@ int main( void )
     uint32_t bms_cmd_param = 0U;
 
     /*TODO Classical: 1. Declare a variable of type SensorFrame */
+    SensorFrame sensor_frame;
 
     /*2. Declare a variable of type SensorFrame to hold the last sent sensor data for logging on change */
     uint32_t heartbeat_counter = 0U;
@@ -103,11 +203,11 @@ int main( void )
     while( 1 )
     {
         /* This is a demo loop to showcase the application running */
-        /* TODO Classical: This is for demonstration purposes only and should be deleted when workshop code is written */
-        if( ( heartbeat_counter % 20U ) == 0U)
+        /* TODO Classical: 2. This is for demonstration purposes only and should be deleted when workshop code is written */
+        /*if( ( heartbeat_counter % 20U ) == 0U)
         {
             printk( "[APP1] classical demo loop\n" );
-        }
+        }*/
 
 
         // Read command for BMS from console and apply it to BMS
@@ -118,29 +218,31 @@ int main( void )
         /*TODO Classical: 3. Call BMS APIs to get the sensor data and fill the SensorFrame struct with the data. 
             -- sensor_frame is a placeholder variable, it is a type of SensorFrame struct that you will define based on the workshop specification
         */
-        // sensor_frame.battery_voltage_mv = (uint32_t)(bms_get_voltage());
-        // sensor_frame.charge_current_ma = (int32_t)(bms_get_current());
-        // sensor_frame.battery_temp_c = bms_get_temperature();
-        // sensor_frame.breaker_closed = bms_get_breaker_closed();
-        // sensor_frame.fault_flags = bms_get_fault_flags();
-
+        sensor_frame.battery_voltage_mv = (uint32_t)(bms_get_voltage());
+        sensor_frame.charge_current_ma = (int32_t)(bms_get_current());
+        sensor_frame.battery_temp_c = bms_get_temperature();
+        sensor_frame.breaker_closed = bms_get_breaker_closed();
+        sensor_frame.fault_flags = bms_get_fault_flags();
 
         /*TODO Classical: 4. Publish/log/send the SensorFrame to the peer using shared memory access (write to defined memory address for SensorFrame)
             -- SENSOR_FRAME_BASE is a memory address where the SensorFrame will be written
             -- Synchronization is important, so make sure to implement a simple protocol to signal when new data is available for the peer to read.
         */
+        if(write_sensor_frame(&sensor_frame)) {
+            
+        }
 
         /*TODO Classical: 5. Log the Info to the console using printk("[APP1] ") which is behaving similar to printf */
         // sensor_frame is a placeholder variable, it is a type of SensorFrame struct that you will define based on the workshop specification
-        // if( ( heartbeat_counter % 10U ) == 0U || sensor_frame.fault_flags != 0U )
-        // {
-        //     printk( "[APP1] CC sent mV=%u mA=%d temp=%f breaker_closed=%u faults=%d\n",
-        //                ( unsigned int ) sensor_frame.battery_voltage_mv,
-        //                sensor_frame.charge_current_ma,
-        //                sensor_frame.battery_temp_c,
-        //                ( unsigned int ) sensor_frame.breaker_closed,
-        //                ( unsigned int ) sensor_frame.fault_flags );
-        // }
+        if( ( heartbeat_counter % 10U ) == 0U || sensor_frame.fault_flags != 0U )
+        {
+            printk( "[APP1] CC sent mV=%u mA=%d temp=%f breaker_closed=%u faults=%d\n",
+                    ( unsigned int ) sensor_frame.battery_voltage_mv,
+                    sensor_frame.charge_current_ma,
+                    sensor_frame.battery_temp_c,
+                    ( unsigned int ) sensor_frame.breaker_closed,
+                    ( unsigned int ) sensor_frame.fault_flags );
+        }
 
         heartbeat_counter++;
         k_msleep( WORKSHOP_SENSOR_PERIOD_MS );
@@ -202,7 +304,7 @@ int main( void )
     uint32_t bms_cmd_id = 0U;
     uint32_t bms_cmd_param = 0U;
 
-    /* TODO HALO: 3. Declare a variable of type SensorFrame generated from HALO
+    /* TODO HALO: 2. Declare a variable of type SensorFrame generated from HALO
       - SensorFrame struct is available from HALO generated code from deps/halo/codegen/riscv64_h1_zephyr/include/halo_structs.h
     */
 
@@ -214,7 +316,7 @@ int main( void )
     {
 
         /* This is a demo loop to showcase the application running */
-        /* TODO HALO: This is for demonstration purposes only and should be deleted when workshop code is written */
+        /* TODO HALO: 3. This is for demonstration purposes only and should be deleted when workshop code is written */
         if( ( heartbeat_counter % 20U ) == 0U)
         {
             printk( "[APP1] HALO demo loop\n" );
